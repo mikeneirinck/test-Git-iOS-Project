ff26790bc023e880e0a5b1ef9ed56ef7c4873813
mikeneirinck <mikeneirinck@Mikes-Mac-mini.local>
2014-11-25 23:53:20 +0100

    second commit

---
+++ GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Mike Neirinck on 25/11/14.
+//  Copyright (c) 2014 mike.neirinck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Mike Neirinck on 25/11/14.
+//  Copyright (c) 2014 mike.neirinck. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
